1a40695e8f2de97d5d139012bebd902cfbb14247
Mohamedsayhii <mohamedsaihii@outlook.fr>
2024-07-07 22:06:29 +0100

    Set up all the UI except for the knight travail button

--- src/index.js
+++ src/index.js
@@ -1,26 +1,58 @@
-import _ from 'lodash';
 import './sass/style.scss';
+import content from './content';
+// eslint-disable-next-line import/no-unresolved
+import icon from './assets/images/knight-chess-icon.png';
 
 if (process.env.NODE_ENV !== 'prodcution') {
     console.log('Looks like we are in development mode!');
 }
 
-export default function printMe() {
-    console.log('Called from print.js!');
-}
+const knightIcon = document.createElement('img');
+knightIcon.src = icon;
 
-function component() {
-    const element = document.createElement('div');
-    const btn = document.createElement('button');
+const placeKnight = () => {
+    const board = document.querySelector('#board');
+    board.addEventListener('click', (e) => {
+        console.log(e.target.className.includes('cell'));
+        if (
+            e.target.className.includes('cell') &&
+            placeKnightBtn.classList.contains('clicked')
+        ) {
+            e.target.appendChild(knightIcon);
+            placeKnightBtn.classList.remove('clicked');
+        }
+    });
+};
 
-    element.innerHTML = _.join(['Hamza'], ' ');
+const randomKnightPlacement = () => {
+    const i = Math.floor(Math.random() * 8) + 1;
+    const j = Math.floor(Math.random() * 8) + 1;
+    const randomCell = document.getElementById(`[${i},${j}]`);
+    if (randomCell.className.includes('cell')) {
+        randomCell.appendChild(knightIcon);
+    }
+};
 
-    btn.innerHTML = 'Click me and check the console!';
-    btn.onclick = printMe;
+const title = document.createElement('h1');
+title.textContent = 'KNIGHTS TRAVAIL';
 
-    element.appendChild(btn);
+const body = document.querySelector('body');
 
-    return element;
-}
+body.appendChild(title);
+body.appendChild(content());
 
-document.body.appendChild(component());
+const placeKnightBtn = document.querySelector('#placeKnightBtn');
+const randomKnightPlacementBtn = document.querySelector(
+    '#randomKnightPlacementBtn',
+);
+
+placeKnightBtn.classList.add('aasba');
+
+placeKnightBtn.addEventListener('click', () => {
+    placeKnightBtn.classList.add('clicked');
+    placeKnight();
+});
+
+randomKnightPlacementBtn.addEventListener('click', () =>
+    randomKnightPlacement(),
+);
